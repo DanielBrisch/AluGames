c82d6dd2036f0e4a7374f54031fab0699fcc0c94
DanielBrisch <danielbrisch157@gmail.com>
2024-05-01 21:35:07 -0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+package org.example
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+
+fun main() {
+    val cliente: HttpClient = HttpClient.newHttpClient();
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146")).build()
+}
